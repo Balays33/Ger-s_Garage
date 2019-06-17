b4107d40fe1e53fe58417d54fd451d83ea29f879
Balays33 <40806995+Balays33@users.noreply.github.com>
2019-06-17 07:18:06 +0100

    website updated

--- DataBaseScript.sql
+++ DataBaseScript.sql
@@ -1,12 +1,15 @@
+drop database Gers_Garage;
+
 Create database  Gers_Garage;
 
 Create table  Gers_Garage.customers(
-						contact_id INT(5000) NOT NULL AUTO_INCREMENT,
+						contact_id INT NOT NULL AUTO_INCREMENT,
                         first_name VARCHAR(25),
 						last_name VARCHAR(30) NOT NULL,
 						birthday DATE, 
                         Mobile_Number int(20) not null,
                         customer_Email varchar(40) not null,
+                        customer_Comment varchar(200),
                         primary key (contact_id)
 );
 
@@ -17,6 +20,29 @@ create table Gers_Garage.vehicle(
                         vehicle_BodyType varchar(15),
                         vehicle_Date int (4),
                         vehicle_Brand varchar (20),
-                        vehicle_LicenseN varchar (10),
-                        vehicle_color varchar (10)
+                        vehicle_LicenseN varchar (10) unique,
+                        vehicle_color varchar (10),
+                        primary key (vehicle_LicenseN)
+);
+
+create table Gers_Garage.Booking(
+						booking_Number int  unique AUTO_INCREMENT,
+                        booking_Service varchar (20),
+                        booking_Cost int (1000),
+                        booking_ExtraCost int (1000),
+                        primary key (booking_Number)
+                        
+);
+
+create table Gers_Garage.Staff(
+						staff_Number int not null auto_increment,
+                        staff_Name varchar (30),
+                        primary key (staff_Number)
+);
+
+create table Gers_Garage.PartItem(
+						partItem_Number int  not null auto_increment,
+                        partItem_Name varchar (30),
+                        partItem_Cost int,
+                        primary key (partItem_Number)
 );
